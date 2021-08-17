654ad5fe91e8fb373ae0c56564c19dfe25bcd7a5
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-17 21:54:33 +0530

    added: welcome message

---
+++ src/com/bridgelabz/linecomparision/Linecomparision.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linecomparision;
+
+public class Linecomparision {
+	public static void main(String [] args) {
+		System.out.println("welcome to line comparision using oops concepts");
+	}
+}
